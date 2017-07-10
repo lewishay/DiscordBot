a66c7b33ef8fa2ae04b1c659abed8b6d6b09b0b9
lewishayqa <lewis.hay@qa.com>
2017-07-10 15:36:46 +0000

    Testing that the token is ignored

--- discordBot.js
+++ discordBot.js
@@ -8,8 +8,25 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === "!attack") {
+    var attackValue = Math.floor(Math.random() * 100) + 1;
+    msg.reply("I attack for " + attackValue + " damage!");
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === "!magic") {
+    var spellList = ["Fire", "Thunder", "Blizzard", "Haste", "Quake"]
+    var spell = spellList[Math.floor(Math.random() * 5)];
+    msg.reply("I cast " + spell + "!");
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === "!item") {
+    var itemList = ["Potion", "Ether", "Megalixir"]
+    var item = itemList[Math.floor(Math.random() * 3)];
+    msg.reply("I use " + item + "!");
   }
 });
 
